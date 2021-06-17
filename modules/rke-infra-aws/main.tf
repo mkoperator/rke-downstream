18bd14c6f38103abc9e8a2dce6e59456549b33ea
Mikhail Kozorovitskiy <mikhail@thecrazyrussian.com>
2021-06-17 13:33:06 -0500

    update vpc creation

--- modules/rke-infra-aws/main.tf
+++ modules/rke-infra-aws/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "node_master" {
   vpc_security_group_ids      = [aws_security_group.master_nodes.id]
   subnet_id                   = element(tolist(data.aws_subnet_ids.available.ids), 0)
   associate_public_ip_address = true
-  user_data                   = local.node_master_cloudinit
+  user_data                   = "${local.node_master_cloudinit} --node-name ${var.prefix}-node-master-${count.index}"
   root_block_device {
     volume_type = "gp2"
     volume_size = "100"
@@ -31,7 +31,7 @@ resource "aws_instance" "node_master" {
     Name                                     = "${var.prefix}-node-master-${count.index}"
     K8sRoles                                 = "controlplane,etcd"
     TFModule                                 = var.prefix
-    "kubernetes.io/cluster/${var.clusterid}" = "shared"
+    "kubernetes.io/cluster/${var.clusterid}" = "owned"
   }
   lifecycle {
     create_before_destroy = true
@@ -45,7 +45,7 @@ resource "aws_instance" "node_svc_worker" {
   vpc_security_group_ids      = [aws_security_group.svc_nodes.id]
   subnet_id                   = element(tolist(data.aws_subnet_ids.available.ids), 0)
   associate_public_ip_address = true
-  user_data                   = local.node_svc_worker_cloudinit
+  user_data                   = "${local.node_svc_worker_cloudinit} --node-name ${var.prefix}-node-svc-${count.index}"
   iam_instance_profile        = var.svc_iam_instance_profile
   root_block_device {
     volume_type = "gp2"
@@ -64,11 +64,11 @@ resource "aws_instance" "node_svc_worker" {
     }
   }
   tags = {
-    Name                                     = "${var.prefix}-node-svc-worker-${count.index}"
+    Name                                     = "${var.prefix}-node-svc-${count.index}"
     K8sRoles                                 = "worker"
     NodeRole                                 = "service"
     TFModule                                 = var.prefix
-    "kubernetes.io/cluster/${var.clusterid}" = "shared"
+    "kubernetes.io/cluster/${var.clusterid}" = "owned"
   }
   lifecycle {
     create_before_destroy = true
@@ -82,7 +82,7 @@ resource "aws_instance" "node_game_worker" {
   vpc_security_group_ids      = [aws_security_group.game_nodes.id]
   subnet_id                   = element(tolist(data.aws_subnet_ids.available.ids), 0)
   associate_public_ip_address = true
-  user_data                   = local.node_game_worker_cloudinit
+  user_data                   = "${local.node_game_worker_cloudinit} --node-name ${var.prefix}-node-game-${count.index}"
   iam_instance_profile        = var.game_iam_instance_profile
   root_block_device {
     volume_type = "gp2"
@@ -101,11 +101,11 @@ resource "aws_instance" "node_game_worker" {
     }
   }
   tags = {
-    Name                                     = "${var.prefix}-node-game-worker-${count.index}"
+    Name                                     = "${var.prefix}-node-game-${count.index}"
     K8sRoles                                 = "worker"
     NodeRole                                 = "game"
     TFModule                                 = var.prefix
-    "kubernetes.io/cluster/${var.clusterid}" = "shared"
+    "kubernetes.io/cluster/${var.clusterid}" = "owned"
   }
   lifecycle {
     create_before_destroy = true
@@ -141,7 +141,7 @@ resource "aws_instance" "node_all" {
     Name                                     = "${var.prefix}-node-aio-${count.index}"
     K8sRoles                                 = "controlplane,etcd,worker"
     TFModule                                 = var.prefix
-    "kubernetes.io/cluster/${var.clusterid}" = "shared"
+    "kubernetes.io/cluster/${var.clusterid}" = "owned"
   }
   lifecycle {
     create_before_destroy = true
